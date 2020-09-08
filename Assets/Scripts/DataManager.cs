4d80cea18d4f9f8d3cd9d922f015119f39b16d0f
James D Vieyra <james@vieyra.co.za>
2020-09-08 21:24:51 +0200

    setup

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour 
+{
+    public dialogueData dialogueData;
+    private string file = "baseTest.json";
+    public void Save()
+    {
+        string json = JsonUtility.ToJson(dialogueData);
+        WriteToFile(file, json);
+    }
+
+    public void Load()
+    {
+        dialogueData = new dialogueData();
+        string json = ReadFromFile(file);
+        Debug.Log(json);
+        dialogueData.CharacterNickname = "b";
+        //JsonUtility.FromJson<dialogueData>(json);
+        JsonUtility.FromJsonOverwrite(json, dialogueData);
+        Debug.Log(dialogueData.CharacterNickname);
+    }
+
+    private void WriteToFile(string fileName, string json)
+    {
+        string path = GetFilePath(fileName);
+
+        FileStream fileStream = new FileStream(path, FileMode.OpenOrCreate);
+
+        using(StreamWriter streamWriter = new StreamWriter(fileStream))
+        {
+            streamWriter.Write(json);
+        }
+    }
+
+    private string ReadFromFile(string fileName) 
+    {
+        string path = GetFilePath(fileName);
+        if(File.Exists(path))
+        {
+            using(StreamReader reader = new StreamReader(path))
+            {
+                string json = reader.ReadToEnd();
+                return json;
+            }
+        }
+        else
+        {
+            Debug.LogWarning("File not found");
+
+            return "";
+        }
+    }
+
+    private string GetFilePath(string FileName)
+    {
+        return Path.Combine(Application.persistentDataPath, FileName);
+    }
+}
---
+++ Assets/Scripts/dialogueData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[Serializable]
+public class dialogueData 
+{
+    public string CharacterNickname
+    {
+        get { return characterNickname; }
+        set{ characterNickname = value; }
+    }
+    private string characterNickname;
+
+    public string DialogueSequence
+    {
+        get { return dialogueSequence; }
+        set { dialogueSequence = value; }
+    }
+    private string dialogueSequence;
+
+    public int LineNumber
+    {
+        get { return lineNumber; }
+        set { lineNumber = value; }
+    }
+    private int lineNumber;
+
+    public string[] CharacterLines
+    {
+        get { return characterLines; }
+        set { characterLines = value; }
+    }
+    private string[] characterLines;
+}
