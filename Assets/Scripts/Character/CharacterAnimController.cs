08ccad33db31ef17d036e46460e2876730a3431c
James D Vieyra <james@vieyra.co.za>
2020-10-23 11:32:54 +0200

    Added mechanim Animation

---
+++ Assets/Scripts/Character/CharacterAnimController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimController : MonoBehaviour
+{
+    #region Attributes
+
+    private Animator animator;
+
+    private const string IDLE_ANIMATION_BOOL = "idle";
+    private const string MOVE_ANIMATION_BOOL = "move";
+
+    #endregion
+
+    #region Animate Functions
+
+    public void AnimateIdle()
+    {
+        Animate(IDLE_ANIMATION_BOOL);
+    }
+    public void AnimateMove()
+    {
+        Animate(MOVE_ANIMATION_BOOL);
+    }
+
+    #endregion
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Animate(string boolName)
+    {
+        DisableOTherAnimations(animator, boolName);
+
+        animator.SetBool(boolName, true);
+    }
+
+    private void DisableOTherAnimations(Animator animator, string animation)
+    {
+        foreach(AnimatorControllerParameter parameter in animator.parameters)
+        {
+            if(parameter.name != animation)
+            {
+                animator.SetBool(parameter.name, false);
+            }
+        }
+    }
+}
--- Assets/Scripts/Character/CharcterController.cs
+++ Assets/Scripts/Character/CharcterController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class CharcterController : MonoBehaviour
 {
     public CharacterController character;
+    public CharacterAnimController characterAnim;
     public float Speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
         character = GetComponent<CharacterController>();
+        characterAnim.AnimateIdle();
     }
 
     // Update is called once per frame
@@ -20,7 +22,15 @@ public class CharcterController : MonoBehaviour
             Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             character.Move(move * Time.deltaTime * Speed);
             if (move != Vector3.zero)
+            {
                 transform.forward = move;
+                characterAnim.AnimateMove();
+            }
+            else
+            {
+                characterAnim.AnimateIdle();
+            }
+                
         }            
     }
 }
