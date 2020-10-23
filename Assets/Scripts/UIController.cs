e85eb43d7b95209059fb9b9c052d9ab7c02c412f
James D Vieyra <james@vieyra.co.za>
2020-10-23 12:09:40 +0200

    Setup basic UI controller

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using TMPro.EditorUtilities;
+
+public class UIController : MonoBehaviour
+{
+    #region Writer
+    public GameObject panel;
+    public TextMeshProUGUI outputText;
+    public TMP_InputField userText;
+    public Button Go;
+    public Button Close;
+    public TextMeshProUGUI txtGO;
+    bool writerVis = false;
+    #endregion
+
+    #region Writer Vis
+    public void OpenWriter()
+    {
+        writerVis = !writerVis;
+        ChangeWriterVis(writerVis);
+    }
+
+    public void CloseWriter()
+    {
+        writerVis = false;
+        ChangeWriterVis(writerVis);
+    }
+
+    void ChangeWriterVis(bool vis)
+    {
+        panel.SetActive(vis);
+        outputText.gameObject.SetActive(vis);
+        userText.gameObject.SetActive(vis);
+        Go.gameObject.SetActive(vis);
+        Close.gameObject.SetActive(vis);
+        txtGO.gameObject.SetActive(vis);
+    }
+    #endregion
+
+    private void Start()
+    {
+        CloseWriter();
+    }
+}
